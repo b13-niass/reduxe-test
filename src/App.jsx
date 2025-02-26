e54107a8bfdd355c7af993e747bbdcf3d5c9adb7
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2025-02-26 16:55:50 +0000

    feat(final): teste de la liste des taches

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Task from "./components/Task.jsx";
+import TaskList from "./components/TaskList.jsx";
 function App() {
 
   return (
-    <>
-
-    </>
+      <>
+        <div className="App">
+          <Task/>
+          <TaskList/>
+        </div>
+      </>
   )
 }
 
